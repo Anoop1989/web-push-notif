8f3728ab44d915987d5722ee46962f1531bcba3b
Kavin <kavin.saratha@gmail.com>
2020-03-05 15:10:51 +0530

    Adding index files

--- app/assets/javascripts/init-firebase.js
+++ app/assets/javascripts/init-firebase.js
@@ -7,3 +7,17 @@ const firebaseConfig = {
     messagingSenderId: "1086162068957",
     appId: "1:1086162068957:web:b0f0c075cd5190b039c234"
   };
+  firebase.initializeApp(firebaseConfig);
+
+  const messaging = firebase.messaging();
+  messaging.requestPermission().then(function(){
+    console.log("Have Permission");
+     console.log(messaging.getToken());
+  }).
+  then(function(){
+    console.log(token);
+  }).
+  catch(function(err){
+    console.log("Error-- ", err);
+  })
+
