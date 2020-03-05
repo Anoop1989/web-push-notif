9716e2065e13aa2211435d20412e60b11e22dfd1
mamatha <dmamtha1@gmail.com>
2020-03-05 15:27:55 +0530

    web push notification

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,20 @@
 class WelcomeController < ApplicationController
+  def index
+  end
 
-    def index
-        
-    end
+  def create
+    AlertReceiver.find_or_create_by!(browser_id: notify_params[:browser_id])
+
+    render json: { message: 'Thanks for subscribing!'}, status: :ok
+  rescue => error
+    Rails.logger.error(error.message)
+    Rails.logger.error(error.backtrace.join('\n'))
+    render json: { message: 'Unable to proceed. Please contact support!'}, status: :forbidden
+  end
+
+  private
+
+  def notify_params
+    params.permit(:browser_id)
+  end
 end
